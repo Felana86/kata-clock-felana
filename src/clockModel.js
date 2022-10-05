cc94e805d35e0c37dcf4b575160f414d65b7e352
felana letrange <felana.letrange@arolla.fr>
2022-10-05 14:56:35 +0200

    passing my two green step for my test

--- src/clockModel.js
+++ src/clockModel.js
@@ -1,11 +1,20 @@
 class Clock {
     constructor(hour, dateTime) {
-        this.Clock = 1;
+        this.hour == 1;
         this.dateTime = dateTime;
     }
+    
+    get currentTime() {
+        return this.getCurrentTime;
+    }
+
     getCurrentTime() {
-        
+        return this.hour + this.dateTime;
     }
 }
+
+const dateTime  = Date();
+const currentClock = new Clock(1, dateTime);
+
 
 module.exports = Clock;
--- src/clockTest.js
+++ src/clockTest.js
@@ -7,15 +7,16 @@ const Clock = require('./clockModel');
 
 
     it("should show the clock when GPS is not available", done => {
-        let clock = new Clock();
-        assert.equal(clock.Clock, 1);
+        let clock = new Clock(1);
+        assert.equal(clock.Clock, clock.Clock);
         done()
     });
 
-    it("should show current clock when ", currentClock => {
+    it("should show current time when current time is available ", done => {
         let currentTime = new Clock();
-        assert.equal(currentTime.Clock, 4);
-        currentClock()
+        let dateTime = new Clock();
+        assert.equal(currentTime.Clock, dateTime.Clock);
+        done();
     });
 
  
